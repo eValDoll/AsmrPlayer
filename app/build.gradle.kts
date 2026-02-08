7b58996ef2657d0174f6cc0c34702b5f54282a38
eValDoll <evaldoll@users.noreply.github.com>
2026-02-09 00:58:59 +0800

    Release v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.asmr.player"
         minSdk = 24
         targetSdk = 34
-        versionCode = 101
-        versionName = "0.1.0"
+        versionCode = 102
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
