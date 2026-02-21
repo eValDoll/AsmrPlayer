f838b891c12a21470442f83f66f2ae2d3d345af9
eValDoll <2934742250@qq.com>
2026-02-22 00:41:00 +0800

    Bump version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.asmr.player"
         minSdk = 24
         targetSdk = 34
-        versionCode = 105
-        versionName = "0.1.4"
+        versionCode = 106
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
